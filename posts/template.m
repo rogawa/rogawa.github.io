ac6946d417d2a8046b62629829c7bb27a761f5a8
Rui Ogawa <ruiogawa@MBA-RUI.local>
2020-09-13 02:22:45 -0400

    rebuilding site Sun Sep 13 02:22:43 -04 2020

---
+++ posts/template.m
@@ -0,0 +1,22 @@
+---
+author:
+  Name: "Rui Ogawa"
+date: 2020-09-13
+linktitle: O pensamento liberal
+title: O pensamento liberal
+type: post
+categories:
+  - Política
+tags:
+  - Pensamento
+  - Liberal
+
+---
+
+O pensamento liberal impede-nos de ver quais são as forças que nos impelem a continuar defendendo-o.
+
+Esse é o paradoxo. De dentro desse pensamento, ele acredita que é racional e certo. Precisa sair e se olhar de fora, para enxergar a que classe pertence.
+
+
+            
+  
